8e14758668c40457f3558c081a082dcfe14d3b5d
vaggelisdrak <71725114+vaggelisdrak@users.noreply.github.com>
2023-05-14 17:17:20 +0300

    Delete PYTHON QUIZ - Copy (4) directory

--- PYTHON QUIZ - Copy (4)/static/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = () =>{
-    const transition_el = document.querySelector(".transition");
-    setTimeout(() => {
-        transition_el.classList.remove('is-active');
-    }, 500);
-}
